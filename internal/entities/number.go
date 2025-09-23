92d08c9be738ba30ca24714a16d6e95753ef056a
sanjar <sanjar@>
2025-09-23 19:37:43 +0500

    feat: implement "number" entity interface

--- internal/entities/number.go
+++ internal/entities/number.go
@@ -1,1 +1,39 @@
 package entities
+
+import (
+	"image/color"
+)
+
+type NumberType int
+
+const (
+	TypeBasic NumberType = iota
+	TypePrime
+	TypeComposite
+)
+
+type Number struct {
+	BaseEntity
+	Value     int
+	Type      NumberType
+	VelocityX float64
+	VelocityY float64
+	Color     color.RGBA
+	IsMoving  bool
+}
+
+func NewNumber(x, y float64, value int) *Number {
+	num := &Number{
+		BaseEntity: BaseEntity{
+			X:      x,
+			Y:      y,
+			Width:  16,
+			Height: 16,
+		},
+		Value:    value,
+		Type:     determineNumberType(value),
+		Color:    getNumberColor(value),
+		IsMoving: false,
+	}
+	return num
+}
